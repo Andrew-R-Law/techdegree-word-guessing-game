3d7bcf4e49abd32d31645a4db9498fe598c3889c
Andrew-R-Law <andy.bob.law91@gmail.com>
2021-08-18 12:52:37 -0700

    Final version
    Restored hearts upon reset and condensed code.

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ let missed = 0;
 const btn_reset = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
 const keyBtns = document.querySelectorAll('.keyrow button');
+const hearts = document.querySelectorAll('.tries img');
+
 
 //Hides the start screen when 'start game' is clicked'
 btn_reset.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
+//Array of phrases for player to guess
 const phrases = [
     'We are going to need a bigger boat',
     'No soup for you',
@@ -26,31 +29,27 @@ function getRandomPhraseAsArray(arr){
     return characters;
 }
 
-//When passed an array, it appends it to the #phrase ul as an li element. If the array item is not a space, the li element receives the class 'letter'.
+//When passed an array, it appends each item to the #phrase ul as an li element. If the array item is not a space, the li element receives the class 'letter'. Else, receives the class 'space'.
 function addPhraseToDisplay (arr) {
     for (i = 0; i < arr.length; i++){
+        const text = arr[i];
+        const ul = document.querySelector('#phrase ul');
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        li.textContent = text;
         if (arr[i] !== ' ') {
-            const text = arr[i];
-            const ul = document.querySelector('#phrase ul');
-            const li = document.createElement('li');
-            li.textContent = text;
             li.className = 'letter';
-            ul.appendChild(li);
         } else {
-            const text = arr[i];
-            const ul = document.querySelector('#phrase ul');
-            const li = document.createElement('li');
-            li.textContent = text;
             li.className = 'space';
-            ul.appendChild(li);
         }
     }
 }
 
-//calls the addPhraseToDisplay function.
+//Gets a random string of characters and calls the addPhraseToDisplay function.
 let phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
+//A function that takes the player's guess as a parameter and shows that letter if it is in the phrase. Otherwise, returns null.
 function checkLetter (playerGuess) {
     const letters = document.getElementsByClassName('letter');
     let correctLetter = null;
@@ -63,41 +62,60 @@ function checkLetter (playerGuess) {
     return correctLetter;
 }
 
+//Stores the winning phrase inside an h3 element to be appended to the overlay screen upon completion of the game.
 const winningPhrase = document.createElement('h3');
 winningPhrase.textContent = `The phrase was "${phraseArray.join('')}."`;
 
+//function that checks whether the player has correctly guessed the phrase or if they are out of hearts.
+//If they correctly guess, then overlay is displayed with a congratulatory phrase as well as the winning phrase.
+//If they run out of hearts, overlay is displayed with a non-congratulatory phrase as well as the winning phrase.
+//Also calls the reset function, allowing the player to play another round when clicking 'play again'.
 function checkWin () {
     let numCorrect = document.querySelectorAll('.show').length;
     let numToGuess = document.querySelectorAll('.letter').length;
+    const title = document.querySelector('.title');
+    const winningPhrase = document.createElement('h3');
+    winningPhrase.textContent = `The phrase was "${phraseArray.join('')}."`;
 
     if (numCorrect === numToGuess) {
         setTimeout(function () {
-            const title = document.querySelector('.title');
             overlay.className = 'win';
             overlay.style.display = '';
             title.textContent = 'Nice Job! You are a winner!';
             btn_reset.innerText = 'Play Again';
-            const winningPhrase = document.createElement('h3');
-            winningPhrase.textContent = `The phrase was "${phraseArray.join('')}."`;
             overlay.appendChild(winningPhrase);
             reset();
         }, 1500);
     }
     if (missed >= 5) {
         setTimeout(function (){ 
-            const title = document.querySelector('.title');
             overlay.className = 'lose';
             overlay.style.display = '';
-            title.textContent = 'Game Over. You are a loser.';
+            title.textContent = 'Game Over. Better luck next time.';
             btn_reset.innerText = 'Try again';
-            const winningPhrase = document.createElement('h3');
-            winningPhrase.textContent = `The phrase was "${phraseArray.join('')}."`;
             overlay.appendChild(winningPhrase);
             reset();
         }, 500);
     }
 }
 
+//Listens for the player's guess as a click on the qwerty display, calls the checkLetter function, and calls the checkWin function.
+for (i = 0; i < keyBtns.length; i++){
+    keyBtns[i].addEventListener('click', (e) => {
+        let playerGuess = e.target.innerText;
+        checkLetter(playerGuess);
+        e.target.className += ' chosen';
+        e.target.disabled = 'true';
+        let letterFound = checkLetter(playerGuess);
+        if (letterFound === null){
+            hearts[missed].src = 'images/lostHeart.png';
+            missed++;
+        }
+        checkWin();
+    });
+}
+
+//function that resets the game when called.
 function reset () {
     btn_reset.addEventListener('click', () => {
         missed = 0;
@@ -105,27 +123,17 @@ function reset () {
             keyBtns[i].className = '';
             keyBtns[i].disabled = false;
         }
+        for (i = 0; i < hearts.length; i++){
+            hearts[i].src = 'images/liveHeart.png';
+        }
         overlay.removeChild(overlay.lastChild);
         winningPhrase.textContent = '';
-        const phraseLetters = document.querySelector('#phrase ul');
-        phraseLetters.innerHTML = '';
+        const ul = document.querySelector('#phrase ul');
+        ul.innerHTML = '';
         phraseArray = getRandomPhraseAsArray(phrases);
         addPhraseToDisplay(phraseArray);
     });
 }
 
-for (i = 0; i < keyBtns.length; i++){
-keyBtns[i].addEventListener('click', (e) => {
-    let playerGuess = e.target.innerText;
-    checkLetter(playerGuess);
-    e.target.className += ' chosen';
-    e.target.disabled = 'true';
-    let letterFound = checkLetter(playerGuess);
-    if (letterFound === null){
-        document.querySelectorAll('.tries img')[missed].src = 'images/lostHeart.png';
-        missed++;
-    }
-    checkWin();
-});
-}
+
 
